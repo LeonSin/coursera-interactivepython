444fe53648a691cf488af9009630b1fe56b65dab
Leon.R.Sine <leonsin112358@gmail.com>
2014-10-17 23:27:36 +0800

    pong update the position of paddle

--- pong.py
+++ pong.py
@@ -22,10 +22,10 @@ def spawn_ball(direction):
     if direction == RIGHT:
         ball_vel = [random.randrange(120, 240) / 60.0, -random.randrange(60, 180) / 60.0]
     elif direction == LEFT:
-        ball_vel = [random.randrange(120, 240) / 60.0, -random.randrange(60, 180) / 60.0]    
+        ball_vel = [-random.randrange(120, 240) / 60.0, -random.randrange(60, 180) / 60.0]    
         
 # define event handlers
-def new_game():
+def new_game(right):
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2  # these are ints
     paddle1_pos = [0 + PAD_WIDTH, HEIGHT / 2.0]
@@ -33,11 +33,11 @@ def new_game():
     paddle1_vel = 0
     paddle2_vel = 0
     
-    spawn_ball(RIGHT)
+    spawn_ball(right)
     
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
-    global paddle1_vel, paddle2_vel
+ 
         
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
@@ -49,18 +49,16 @@ def draw(canvas):
             ball_vel[0] = -ball_vel[0]
             velocityIncrease()
         else:
-            new_game()
+            new_game(RIGHT)
     elif ball_pos[0] >= WIDTH - BALL_RADIUS:
-        print 'bong right'
         if ball_pos[1] > (paddle2_pos[1] - HALF_PAD_HEIGHT) and ball_pos[1] < (paddle2_pos[1] + HALF_PAD_HEIGHT):
             ball_vel[0] = -ball_vel[0]
             velocityIncrease()
         else:
-            new_game()  
+            new_game(LEFT)  
     ball_pos[0] += ball_vel[0]
         
     if ball_pos[1] <= BALL_RADIUS or ball_pos[1] >= HEIGHT - BALL_RADIUS:
-        print 'bong'
         ball_vel[1] = - ball_vel[1]
         
     ball_pos[1] += ball_vel[1]
@@ -69,35 +67,23 @@ def draw(canvas):
     canvas.draw_circle( ball_pos, BALL_RADIUS, 1, 'White', 'White')
     
     # update paddle's vertical position, keep paddle on the screen
-    if paddle1_pos[1] - HALF_PAD_HEIGHT < 0:
-        paddle1_pos[1] = HALF_PAD_HEIGHT
-    elif paddle1_pos[1] + HALF_PAD_HEIGHT > HEIGHT:
-        paddle1_pos[1] = HEIGHT - HALF_PAD_HEIGHT
-    else:
-        paddle1_pos[1] += paddle1_vel
-    
-    if paddle2_pos[1] - HALF_PAD_HEIGHT < 0:
-        paddle2_pos[1] = HALF_PAD_HEIGHT
-    elif paddle2_pos[1] + HALF_PAD_HEIGHT > HEIGHT:
-        paddle2_pos[1] = HEIGHT - HALF_PAD_HEIGHT
-    else:
-        paddle2_pos[1] += paddle2_vel
-    
+    paddle1_pos[1] += paddle1_vel
+    paddle2_pos[1] += paddle2_vel
     # draw paddles
-    canvas.draw_line([0, paddle1_pos[1] - HALF_PAD_HEIGHT],[0, paddle1_pos[1] + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
-    canvas.draw_line([WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT],[WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
+    canvas.draw_line([0 + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT],[0 + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
+    canvas.draw_line([WIDTH - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT],[WIDTH - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
     # draw scores
         
 def keydown(key):
     global paddle1_vel, paddle2_vel
     if key == simplegui.KEY_MAP['w']:
-        paddle1_vel = -4
+        paddle1_vel = -2
     if key == simplegui.KEY_MAP['s']:
-        paddle1_vel = 4
+        paddle1_vel = 2
     if key == simplegui.KEY_MAP['up']:
-        paddle2_vel = -4
+        paddle2_vel = -2
     if key == simplegui.KEY_MAP['down']:
-        paddle2_vel = 4
+        paddle2_vel = 2
         
 def keyup(key):
     global paddle1_vel, paddle2_vel
@@ -113,11 +99,12 @@ def velocityIncrease():
 
 # create frame
 frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
+
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
 
 
 # start frame
-new_game()
+new_game(RIGHT)
 frame.start()
